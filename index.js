85afacb7fe89105294c1268d1314f60543f98e40
Cody Partington <codythatsme@gmail.com>
2022-11-19 22:16:10 +1100

    forgot to run build

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const cors_1 = __importDefault(require("@fastify/cors"));
 const distance_1 = __importDefault(require("./utils/distance"));
 const postcodes = require("./postcodes.json");
 const port = process.env.PORT || 4000;
 const server = (0, fastify_1.default)();
-server.get("/api/v1", async (req, res) => {
-    res.send({ message: "Please use POST with 'latitude', 'longitude' and 'radius' in the body as numbers to return postcodes within radius" });
+server.register(cors_1.default, {
+// put your options here
+});
+server.get("/", async (req, res) => {
+    res.send({
+        message: "Please use GET to /api/v1/postcodes/{latitude}/{longitude}/{radius} to return postcodes within radius",
+    });
 });
 server.get("/api/v1/postcodes/:latitude/:longitude/:radius", async (req, res) => {
     const { latitude, longitude, radius } = req.params;
-    console.log(typeof longitude, latitude, radius);
     const matchingPostcodes = postcodes
         .filter((postcode) => {
         return (0, distance_1.default)(postcode.lat, postcode.lng, latitude, longitude) < radius ? true : false;
